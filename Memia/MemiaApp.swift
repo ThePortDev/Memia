a4601c293264560495d4cf102b99ad422e7b5920
ThePortDev <111320669+ThePortDev@users.noreply.github.com>
2022-11-08 14:32:05 -0700

    Initial Commit

---
+++ Memia/MemiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemiaApp.swift
+//  Memia
+//
+//  Created by Porter Dover on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MemiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
