201a156748a235e1e731c87034129eb5d27027a7
ThePortDev <111320669+ThePortDev@users.noreply.github.com>
2022-11-11 20:00:40 -0700

    Revert "stuff"
    This reverts commit 9c6143a5d222238b0e46dfd2488cd8f8c8b39dfa.

--- Memia/View/TriviaGameView.swift
+++ Memia/View/TriviaGameView.swift
@@ -9,11 +9,6 @@ import SwiftUI
 
 struct TriviaGameView: View {
     
-<<<<<<< Updated upstream
-=======
-    @StateObject private var viewModel = MainMenuViewModel()
-        
->>>>>>> Stashed changes
     var body: some View {
         
         NavigationView {
@@ -38,73 +33,6 @@ struct TriviaGameView: View {
             }
         }.accentColor(.black)
     }
-<<<<<<< Updated upstream
-=======
-    
-    var currencyView: some View {
-        NavigationLink(destination: ShopView(viewModel: viewModel).coinShop) {
-            Text("Coins: " + "\(viewModel.coins)")
-                .font(.title3)
-                .padding()
-                .foregroundColor(.black)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .style(strokeColor: .black, strokeWidth: 3, fill: .purple)
-                )
-        }
-    }
-    
-    var twitchView: some View {
-        NavigationLink(destination: TwitchView()) {
-            Text("Twitch Quiz")
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.black)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .style(strokeColor: .black, strokeWidth: 3, fill: .purple)
-                )
-        }
-    }
-    
-    var redditView: some View {
-        NavigationLink(destination: RedditView()) {
-            Text("Reddit Quiz")
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.black)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .style(strokeColor: .black, strokeWidth: 3, fill: .red)
-                )
-        }
-    }
-    var shopView: some View {
-        NavigationLink(destination: ShopView(viewModel: viewModel)) {
-            Text("Shop 🛒")
-                .font(.title3)
-                .padding()
-                .foregroundColor(.black)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .style(strokeColor: .black, strokeWidth: 3, fill: .yellow)
-                )
-        }
-    }
-    var acronymView: some View {
-        NavigationLink(destination: AcronymView()) {
-            Text("Acronym Quiz \(!viewModel.paid ? "🔒" : "")")
-                .padding()
-                .font(.largeTitle)
-                .foregroundColor(.black)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .style(strokeColor: .black, strokeWidth: 3, fill: LinearGradient(colors: [.blue, .yellow, .red, .orange, .purple, .cyan, .green, .mint, .pink], startPoint: .leading, endPoint: .trailing))
-                )
-                .opacity(!viewModel.paid ? 0.5 : 1)
-        } .disabled(!viewModel.paid)
-    }
->>>>>>> Stashed changes
 }
 
 struct ContentView_Previews: PreviewProvider {
