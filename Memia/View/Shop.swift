67a3ed9b231003252c67b47e8d76c876b8651167
ThePortDev <111320669+ThePortDev@users.noreply.github.com>
2022-11-11 20:20:40 -0700

    Hopefully this works

---
+++ Memia/View/Shop.swift
@@ -0,0 +1,59 @@
+//
+//  Shop.swift
+//  Memia
+//
+//  Created by Porter Dover on 11/11/22.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    
+    @ObservedObject var viewModel: MainMenuViewModel
+    
+    var body: some View {
+        NavigationLink(destination: coinShop) {
+            Text("Buy Coins:")
+                .font(.largeTitle)
+                .font(.title3)
+                .padding()
+                .foregroundColor(.black)
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                        .style(strokeColor: .black, strokeWidth: 3, fill: .purple)
+                )
+        }
+        Spacer()
+        Text("ANY PURCHASE WILL PERMANENTLY REMOVE ADS.")
+            .font(.title3)
+            .padding()
+            .foregroundColor(.black)
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .style(strokeColor: .black, strokeWidth: 3, fill: .purple)
+            )
+            .bold()
+            .frame(alignment: .bottom)
+            .multilineTextAlignment(.center)
+        
+    }
+    
+    var coinShop: some View {
+        VStack {
+            Text("This is a coin shop")
+            Button {
+                viewModel.getPaid()
+            } label: {
+                Text("Rob Us")
+                    .font(.title3)
+                    .padding()
+                    .foregroundColor(.black)
+                    .background(
+                        RoundedRectangle(cornerRadius: 15)
+                            .style(strokeColor: .black, strokeWidth: 3, fill: .purple)
+                    )
+            }
+        }
+    }
+    
+}
